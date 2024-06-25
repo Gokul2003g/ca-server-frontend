1ea3a699b3e006eacc5d3340b65bd64bb2d50f7a
Gokul <75162131+Gokul2003g@users.noreply.github.com>
2024-06-25 18:47:59 +0530

    Create UI without functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { Button } from "./components/ui/button"
+import { Label } from "./components/ui/label"
+import { Separator } from "./components/ui/separator"
+import { Textarea } from "./components/ui/textarea"
 
 function App() {
 
+  const generateCertificate = () => {
+
+  }
+
   return (
-    <>
-    </>
+    <div className="flex flex-col items-center py-32 bg-gray-300 min-h-screen w-full">
+      <div className="w-3/4">
+        <Label className="text-3xl">Upload Public Key Here</Label>
+        <Textarea className="my-8 text-xl" placeholder="Upload your public key to be signed" />
+        <Button >Generate certificate</Button>
+      </div>
+      <Separator className="my-16 w-4/5" />
+      <div className="w-3/4">
+        <Label className="text-3xl">Get Certificate Here</Label>
+        <Textarea className="my-8 text-xl" placeholder="Upload your public key to be signed" />
+        <Button>Download Certificate</Button>
+      </div>
+      <Separator className="my-16 w-4/5" />
+      <div className="w-3/4 flex justify-around">
+        <Button>Download User Signing Key</Button>
+        <Button>Download Host Signing Key</Button>
+      </div>
+    </div >
   )
 }
 
