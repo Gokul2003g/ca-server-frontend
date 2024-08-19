9926b89ed51ad059afcd7e218c7c5c3cfe27c367
Gokul <75162131+Gokul2003g@users.noreply.github.com>
2024-08-19 21:27:55 +0530

    Complete login page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -5,11 +5,13 @@ import { Button } from "@/components/ui/button";
 export default function SignIn() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center gap-4">
-      <Button onClick={() => signIn("google", { callbackUrl: "/" })} >
-        SignIn with Google
+      <Button className="max-w-60 w-full h-12 gap-2" onClick={() => signIn("google", { callbackUrl: "/" })} >
+        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 50 50">
+          <path d="M 25.996094 48 C 13.3125 48 2.992188 37.683594 2.992188 25 C 2.992188 12.316406 13.3125 2 25.996094 2 C 31.742188 2 37.242188 4.128906 41.488281 7.996094 L 42.261719 8.703125 L 34.675781 16.289063 L 33.972656 15.6875 C 31.746094 13.78125 28.914063 12.730469 25.996094 12.730469 C 19.230469 12.730469 13.722656 18.234375 13.722656 25 C 13.722656 31.765625 19.230469 37.269531 25.996094 37.269531 C 30.875 37.269531 34.730469 34.777344 36.546875 30.53125 L 24.996094 30.53125 L 24.996094 20.175781 L 47.546875 20.207031 L 47.714844 21 C 48.890625 26.582031 47.949219 34.792969 43.183594 40.667969 C 39.238281 45.53125 33.457031 48 25.996094 48 Z"></path>
+        </svg>       Sign-In with Google
       </Button>
-      <Button onClick={() => signIn("github", { callbackUrl: "/" })}>
-        SignIn with Github
+      <Button className="max-w-60 w-full h-12 gap-2" onClick={() => signIn("github", { callbackUrl: "/" })}>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-github"><path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" /><path d="M9 18c-4.51 2-5-2-7-2" /></svg>  Sign-In with Github
       </Button>
     </div >
   )
